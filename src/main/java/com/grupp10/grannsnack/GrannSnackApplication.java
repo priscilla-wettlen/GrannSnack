2bc686d69256c21fa781b71b6dd43700e847d4de
Joel Seger <joel.allarp@gmail.com>
2025-03-20 14:36:07 +0100

    Initial commit: Spring Boot Project

---
+++ src/main/java/com/grupp10/grannsnack/GrannSnackApplication.java
@@ -0,0 +1,13 @@
+package com.grupp10.grannsnack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrannSnackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrannSnackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupp10/grannsnack/GrannSnackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp10.grannsnack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrannSnackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
