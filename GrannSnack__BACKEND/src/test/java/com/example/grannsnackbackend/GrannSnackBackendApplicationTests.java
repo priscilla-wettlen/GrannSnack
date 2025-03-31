1fbedb5d759f28da1382ec6256c372990b0a7519
Priscilla Silva <priscilla.sg91@gmail.com>
2025-04-01 00:03:50 +0200

    added spring initializr for backend

---
+++ GrannSnack__BACKEND/src/test/java/com/example/grannsnackbackend/GrannSnackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grannsnackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrannSnackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
