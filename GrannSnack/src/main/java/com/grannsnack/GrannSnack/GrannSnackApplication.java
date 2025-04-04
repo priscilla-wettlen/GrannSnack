119058552335575c6a1ece1ff6ba77d0730583e2
Joel Seger <joel.allarp@gmail.com>
2025-04-04 12:06:20 +0200

    new Main

---
+++ GrannSnack/src/main/java/com/grannsnack/GrannSnack/GrannSnackApplication.java
@@ -0,0 +1,13 @@
+package com.grannsnack.GrannSnack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrannSnackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrannSnackApplication.class, args);
+	}
+
+}
---
+++ GrannSnack/src/test/java/com/grannsnack/GrannSnack/GrannSnackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grannsnack.GrannSnack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrannSnackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
