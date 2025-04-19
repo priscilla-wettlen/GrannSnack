d5a16cad449e9363df932a9722f890db0b19da61
Priscilla Silva <priscilla.sg91@gmail.com>
2025-04-19 17:11:25 +0200

    post changes from wednesday

--- src/main/java/com/grannsnack/GrannSnack/WebController/ForumController.java
+++ src/main/java/com/grannsnack/GrannSnack/WebController/ForumController.java
@@ -2,8 +2,10 @@ package com.grannsnack.GrannSnack.WebController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/posts")
 public class ForumController {
 
     @GetMapping("/u/forum")
