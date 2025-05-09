13708b16bbabbddac6a23637adcc5ce3448d673d
Joel Seger <joel.allarp@gmail.com>
2025-05-09 16:32:23 +0200

    s

---
+++ src/main/java/com/grannsnack/GrannSnack/WebController/AdminRestController.java
@@ -0,0 +1,71 @@
+package com.grannsnack.GrannSnack.WebController;
+import com.grannsnack.GrannSnack.Model.MyUser;
+import com.grannsnack.GrannSnack.Model.Post;
+import com.grannsnack.GrannSnack.Service.DBForumService;
+import com.grannsnack.GrannSnack.Service.DBUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/a/home")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminRestController {
+    private final DBUserService dbUserService;
+    private final DBForumService dbForumService;
+
+    public AdminRestController(DBUserService dbUserService, DBForumService dbForumService) {
+        this.dbUserService = dbUserService;
+        this.dbForumService = dbForumService;
+    }
+
+    @GetMapping("/users")
+    public List<MyUser> getAllUsers(Integer userId) {
+        List<MyUser> users = new ArrayList<>();
+        if (userId != null) {
+            MyUser user = dbUserService.getUserById(userId);
+            if (user != null && !user.getRole().equals("ADMIN")) {
+                users.add(user);
+            }
+        }
+        return (List<MyUser>) dbUserService.getUsers();
+    }
+
+
+    @DeleteMapping("/delete-user/{id}")
+    public ResponseEntity<String> deleteUser (@PathVariable Integer id){
+        MyUser userIdToDelete = dbUserService.removeUser(id);
+        List<Post> userPosts = dbForumService.findPostByPostAuthorID(userIdToDelete.getId());
+
+        for(Post post : userPosts) {
+            dbForumService.deletePostById(post.getPostId());
+        }
+        if (userIdToDelete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok("User with id " + id + " deleted successfully.");
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> getAllReportedPosts() {
+        List<Post> posts;
+        posts = dbForumService.findPostsByReported(true);
+        if(posts.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(posts);
+        }
+    }
+
+    @DeleteMapping("/posts/{postId}")
+    public ResponseEntity<String> deletePost (@PathVariable Integer postId){
+       Post postToDelete = dbForumService.getPostById(postId);
+       dbForumService.deletePostById(postToDelete.getPostId());
+        return ResponseEntity.ok("Post with id " + postId + " deleted successfully.");
+    }
+}
+
